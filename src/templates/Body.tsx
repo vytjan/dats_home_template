754da0eb9edbb61f739b37d447281e221bf85327
Donatas <donatas.sledzius@gmail.com>
2023-02-11 16:47:30 +0200

    Feb changes push

--- src/templates/Body.tsx
+++ src/templates/Body.tsx
@@ -3,33 +3,34 @@ import { About } from './About';
 import { Additional } from './Additional';
 import { Attributes } from './Attributes';
 import { Box } from './Box';
-import { Calendar } from './Calendar';
+// import { Calendar } from './Calendar';
 import { ContactForm } from './ContactForm';
-import { Events } from './Events';
+// import { Events } from './Events';
 import { FaqCont } from './Faq';
-import { FloraWeek } from './FloraWeek';
+// import { FloraWeek } from './FloraWeek';
 import { FooterArt } from './FooterArt';
+import { HeaderArt } from './HeaderArt';
 import { HeaderMenu } from './HeaderMenu';
 // import { HallOfFame } from './HallOfFame';
 import { InstaEmbed } from './InstaEmbed';
-import { Logo } from './Logo';
+// import { Logo } from './Logo';
 import { MediumWidget } from './MediumWidget';
 import { Profiles } from './Profiles';
 import { Roadmap } from './Roadmap';
 import { SeenOn } from './SeenOn';
-import { Stats } from './Stats';
-import { Update } from './Update';
+// import { Stats } from './Stats';
+// import { Update } from './Update';
 
 const Body = () => {
   // const router = useRouter();
 
   // console.log(router.basePath);
 
-  const logoProps = {
-    image: `/assets/images/logo.svg`,
-    imageAlt: `Logo`,
-    size: 100,
-  };
+  // const logoProps = {
+  //  image: `/assets/images/logo.svg`,
+  //  imageAlt: `Logo`,
+  //  size: 100,
+  // };
 
   const InstaProps = {
     image: `/assets/images/instagram.png`,
@@ -49,14 +50,14 @@ const Body = () => {
     size: 44,
     title: `Snapshot`,
   };
-  const eventsProps = {
-    description: `Blockchain Futurist conference 2022`,
-    image: `/assets/images/icons/calendar.svg`,
-    imageAlt: `calendar`,
-    date: `August 8-10`,
-    location: `Toronto, Canada`,
-    size: 25,
-  };
+  // const eventsProps = {
+  //  description: `Blockchain Futurist conference 2022`,
+  //  image: `/assets/images/icons/calendar.svg`,
+  //  imageAlt: `calendar`,
+  //  date: `August 8-10`,
+  //  location: `Toronto, Canada`,
+  //  size: 25,
+  // };
 
   const mediumProps = {
     title: `Daturian Structure`,
@@ -83,16 +84,17 @@ const Body = () => {
     metadata that tells a unique story about your
     Daturian.`,
     image: `/assets/images/attributes_img.png`,
+    imageLayers: `/assets/images/attributes_layers.png`,
     imageAlt: `attributes image`,
     size: 150,
   };
 
-  const statsProps = {
-    src: `https://dune.com/embeds/753447/1358291/4ad0a3fb-6ec2-4c83-b3ab-425e10c72d8b`,
-    height: 200,
-    width: 700,
-    title: `Daturians Minted`,
-  };
+  // const statsProps = {
+  //  src: `https://dune.com/embeds/753447/1358291/4ad0a3fb-6ec2-4c83-b3ab-425e10c72d8b`,
+  //  height: 200,
+  //  width: 700,
+  //  title: `Daturians Minted`,
+  // };
 
   const addProps = {
     urlUkraine: `https://opensea.io/collection/daturians4ukraine`,
@@ -128,10 +130,9 @@ const Body = () => {
   return (
     <Section>
       <HeaderMenu></HeaderMenu>
+      <HeaderArt></HeaderArt>
       <div className="flex-col">
         <div className="left-content grid grid-cols-3 gap-5 max-auto px-3">
-          <Logo image={logoProps.image} imageAlt={logoProps.imageAlt}></Logo>
-          <FloraWeek></FloraWeek>
           <MediumWidget
             title={mediumProps.title}
             description={mediumProps.description}
@@ -152,16 +153,6 @@ const Body = () => {
           ></Roadmap>
         </div>
         <div className="right-content grid grid-cols-2 gap-5 max-auto px-3">
-          <Calendar></Calendar>
-          <Update></Update>
-          <Events
-            description={eventsProps.description}
-            location={eventsProps.location}
-            date={eventsProps.date}
-            image={eventsProps.image}
-            imageAlt={eventsProps.imageAlt}
-            size={eventsProps.size}
-          ></Events>
           <div className="insta-attributes col-span-2 gap-5">
             <InstaEmbed
               image={InstaProps.image}
@@ -171,16 +162,11 @@ const Body = () => {
               description1={attributesProps.description1}
               description2={attributesProps.description2}
               image={attributesProps.image}
+              imageLayers={attributesProps.imageLayers}
               imageAlt={attributesProps.imageAlt}
               size={attributesProps.size}
             ></Attributes>
           </div>
-          <Stats
-            src={statsProps.src}
-            height={statsProps.height}
-            width={statsProps.width}
-            title={statsProps.title}
-          ></Stats>
         </div>
       </div>
       <div className="bottom-content grid grid-cols-4 gap-5 max-auto px-3">
