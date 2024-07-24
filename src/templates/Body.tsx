c456329d1f264894033970d56fcd4e6da84569a4
Vytautas Jankauskas <vytas00@gmail.com>
2024-07-24 20:50:59 +0300

    fixed some more formatting

--- src/templates/Body.tsx
+++ src/templates/Body.tsx
@@ -1,23 +1,23 @@
 import { Section } from '../layout/Section';
-import { About } from './About';
-import { Additional } from './Additional';
-import { Attributes } from './Attributes';
-import { Box } from './Box';
+// import { About } from './About';
+// import { Additional } from './Additional';
+// import { Attributes } from './Attributes';
+// import { Box } from './Box';
 // import { Calendar } from './Calendar';
-import { ContactForm } from './ContactForm';
+// import { ContactForm } from './ContactForm';
 // import { Events } from './Events';
-import { FaqCont } from './Faq';
+// import { FaqCont } from './Faq';
 // import { FloraWeek } from './FloraWeek';
-import { FooterArt } from './FooterArt';
+// import { FooterArt } from './FooterArt';
 import { HeaderArt } from './HeaderArt';
 import { HeaderMenu } from './HeaderMenu';
 // import { HallOfFame } from './HallOfFame';
-import { InstaEmbed } from './InstaEmbed';
+// import { InstaEmbed } from './InstaEmbed';
 // import { Logo } from './Logo';
-import { MediumWidget } from './MediumWidget';
-import { Profiles } from './Profiles';
-import { Roadmap } from './Roadmap';
-import { SeenOn } from './SeenOn';
+// import { MediumWidget } from './MediumWidget';
+// import { Profiles } from './Profiles';
+// import { Roadmap } from './Roadmap';
+// import { SeenOn } from './SeenOn';
 // import { Stats } from './Stats';
 // import { Update } from './Update';
 
@@ -32,24 +32,24 @@ const Body = () => {
   //  size: 100,
   // };
 
-  const InstaProps = {
-    image: `/assets/images/instagram.gif`,
-    imageAlt: `instagram`,
-    size: 100,
-  };
+  // const InstaProps = {
+  //   image: `/assets/images/instagram.gif`,
+  //   imageAlt: `instagram`,
+  //   size: 100,
+  // };
 
-  const aboutProps = {
-    image: `/assets/images/icons/twitter.svg`,
-    imageAlt: `twitter`,
-    size: 44,
-    title: `Twitter`,
-  };
-  const roadmapProps = {
-    image: `/assets/images/icons/snapshot.svg`,
-    imageAlt: `snapshot`,
-    size: 44,
-    title: `Snapshot`,
-  };
+  // const aboutProps = {
+  //   image: `/assets/images/icons/twitter.svg`,
+  //   imageAlt: `twitter`,
+  //   size: 44,
+  //   title: `Twitter`,
+  // };
+  // const roadmapProps = {
+  //   image: `/assets/images/icons/snapshot.svg`,
+  //   imageAlt: `snapshot`,
+  //   size: 44,
+  //   title: `Snapshot`,
+  // };
   // const eventsProps = {
   //  description: `Blockchain Futurist conference 2022`,
   //  image: `/assets/images/icons/calendar.svg`,
@@ -59,35 +59,35 @@ const Body = () => {
   //  size: 25,
   // };
 
-  const mediumProps = {
-    title: `Daturian Structure`,
-    description: `Update 2.1`,
-    image: `/assets/images/icons/medium.svg`,
-    imageAlt: `medium`,
-    size: 44,
-    link: `https://www.medium.com`,
-  };
+  // const mediumProps = {
+  //   title: `Daturian Structure`,
+  //   description: `Update 2.1`,
+  //   image: `/assets/images/icons/medium.svg`,
+  //   imageAlt: `medium`,
+  //   size: 44,
+  //   link: `https://www.medium.com`,
+  // };
 
-  const attributesProps = {
-    description1: `Since Daturians are technically advanced creatures
-    some of them have tech features. You ll see in the
-    gallery whether your Daturian belongs to the Natural
-    or Tech faction. Based on that expect events and
-    other activities in our Discord to see which group
-    takes the cake as the best one. But do remember that
-    cake is a lie.`,
-    description2: `There are 222 hand drawn attributes that make up a
-    Daturian! It s all vector and there s no AI. Except for
-    Adobe Illustrator and some coding, of course. And just
-    like Shrek and onions they are made of layers! 8
-    layers, to be exact. Each layer is tied to specific
-    metadata that tells a unique story about your
-    Daturian.`,
-    image: `/assets/images/attributes_img.png`,
-    imageLayers: `/assets/images/attributes_layers.png`,
-    imageAlt: `attributes image`,
-    size: 150,
-  };
+  // const attributesProps = {
+  //   description1: `Since Daturians are technically advanced creatures
+  //   some of them have tech features. You ll see in the
+  //   gallery whether your Daturian belongs to the Natural
+  //   or Tech faction. Based on that expect events and
+  //   other activities in our Discord to see which group
+  //   takes the cake as the best one. But do remember that
+  //   cake is a lie.`,
+  //   description2: `There are 222 hand drawn attributes that make up a
+  //   Daturian! It s all vector and there s no AI. Except for
+  //   Adobe Illustrator and some coding, of course. And just
+  //   like Shrek and onions they are made of layers! 8
+  //   layers, to be exact. Each layer is tied to specific
+  //   metadata that tells a unique story about your
+  //   Daturian.`,
+  //   image: `/assets/images/attributes_img.png`,
+  //   imageLayers: `/assets/images/attributes_layers.png`,
+  //   imageAlt: `attributes image`,
+  //   size: 150,
+  // };
 
   // const statsProps = {
   //  src: `https://dune.com/embeds/753447/1358291/4ad0a3fb-6ec2-4c83-b3ab-425e10c72d8b`,
@@ -96,36 +96,36 @@ const Body = () => {
   //  title: `Daturians Minted`,
   // };
 
-  const addProps = {
-    urlUkraine: `https://opensea.io/collection/daturians4ukraine`,
-    urlCafe: `https://opensea.io/collection/daturians-cafe`,
-    urlGreenhouse: `https://opensea.io/collection/daturians-greenhouse`,
-    imageUkraine: `/assets/images/ukraine.png`,
-    imageUkraineAlt: `Ukraine`,
-    imageCafe: `/assets/images/cafe.png`,
-    imageCafeAlt: `Cafe`,
-    imageGreenhouse: `/assets/images/greenhouse.png`,
-    imageGreenhouseAlt: `Greenhouse`,
-    description: `Daturian Collections on
-    OpenSea created to support
-    Ukraine, create friendships
-    with other projects and to
-    create something special
-    just for YOU!`,
-  };
+  // const addProps = {
+  //   urlUkraine: `https://opensea.io/collection/daturians4ukraine`,
+  //   urlCafe: `https://opensea.io/collection/daturians-cafe`,
+  //   urlGreenhouse: `https://opensea.io/collection/daturians-greenhouse`,
+  //   imageUkraine: `/assets/images/ukraine.png`,
+  //   imageUkraineAlt: `Ukraine`,
+  //   imageCafe: `/assets/images/cafe.png`,
+  //   imageCafeAlt: `Cafe`,
+  //   imageGreenhouse: `/assets/images/greenhouse.png`,
+  //   imageGreenhouseAlt: `Greenhouse`,
+  //   description: `Daturian Collections on
+  //   OpenSea created to support
+  //   Ukraine, create friendships
+  //   with other projects and to
+  //   create something special
+  //   just for YOU!`,
+  // };
 
-  const contactProps = {
-    description: `Have something to say to us? A
-    proposal? A new recipe for a killer
-    cake?`,
-    linkUrl: `daturians.com`,
-  };
+  // const contactProps = {
+  //   description: `Have something to say to us? A
+  //   proposal? A new recipe for a killer
+  //   cake?`,
+  //   linkUrl: `daturians.com`,
+  // };
 
-  const boxProps = {
-    xl: true,
-    image: `/assets/images/polygon_partnership.png`,
-    imageAlt: `Logo`,
-  };
+  // const boxProps = {
+  //   xl: true,
+  //   image: `/assets/images/polygon_partnership.png`,
+  //   imageAlt: `Logo`,
+  // };
 
   return (
     <Section>
